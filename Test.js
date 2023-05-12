9d87bb1208d9b2b65d85b9ea6fac07a06bf917e7
azamat <dinaev2002@gmail.com>
2023-05-12 17:49:39 +0300

    usestrict

--- Test.js
+++ Test.js
@@ -1,30 +1,4 @@
-const city = "Moscow"
-const country = "Russia"
 
-const objectA = {
-	city,
-	country,
-	func(ad) {
-		return 3
-	},
-	objectB:{
-		home: "house"
-	}
-}
+f = (b) => 22222	
 
-
-
-const objectC = {...objectA}
-
-objectA.func = () => {
-
-	return 10
-}
-
-
-
-f1 = (obj) => Object.assign({}, obj)
-
-const objectD = f1(objectA)
-objectD.objectB = "asdasd"
-console.dir(objectD)
+console.log(f)

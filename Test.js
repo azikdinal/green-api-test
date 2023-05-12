4227b77fa2e5e04136a33bec2ad3df780b3ef3f0
azamat <dinaev2002@gmail.com>
2023-05-12 11:15:47 +0300

    functions

--- Test.js
+++ Test.js
@@ -1,24 +1,7 @@
-const visible = null
-
-
-let some = {
-	visible: true,
-	colorDepth:24,
-	title:"My image",
-	orientation:{
-		angle:0,
-		type:'landscape'
-	},
-	pixelDepth: 24,
-	width:1440
+let a = () =>{
+	console.log('asdasd')
 }
 
-let a = 1111111
+a = 'sadas'
 
-let non = a
-
-a = 22222
-
-some.width = 32423
-
-console.log(non)
+console.log(a())

4fb49e992f8a465755e1184f8d345582ab5f6ee9
azamat <dinaev2002@gmail.com>
2023-05-18 11:51:39 +0300

    set up line breaker

--- src/components/Message.js
+++ src/components/Message.js
@@ -3,14 +3,14 @@ import React from 'react';
 const Message = (props) => {
 
     const str = 'И предупредите, чтобы немного смекалки проявили коллеги, чтобы не все ИЗ и отчёты руководителя были одинаковые ( про ответственность, внимательность можно дополнить, можно недостатки или замечания мелкие написать).'
-    const breakesStr = ''
+
+    const strs = str.split('\n').filter(s => s);
 
     return (
         <div className='message' style={{justifyContent:props.isUser ? 'flex-end' : 'flex-start'}}>
             <div className='rectangle' style={{backgroundColor: props.isUser ? '#98d572' : '#424242FF'}}>
-                asdasdasdasdasdasdasdasdasdasdasdasd
+                {strs.map(e => <>{e}<br/></>)}
             </div>
-
 
             <div></div>
         </div>

c2f932a1e278e9fa32b47d4fb6ef91e4c9ac8246
azamat <dinaev2002@gmail.com>
2023-05-21 21:02:28 +0300

    added popup

--- src/components/modal/Modal.js
+++ src/components/modal/Modal.js
@@ -1,10 +1,16 @@
-import React from 'react';
+import React, {useState} from 'react';
 
-const Modal = ({active, setActive}) => {
+const Modal = () => {
+    const [active, setActive] = useState(true)
+
+
     return (
-        <div className="modal active">
-            <div className="modal__content" onClick={e => e.stopPropagation()}>
-
+        <div className={active ? 'modal' : 'none'} onClick={() => setActive(false)}>
+            <div className="content" onClick={e => e.stopPropagation()}>
+                <div>Enter your data</div>
+                <input type="text" placeholder="idInstance"/>
+                <input type="text" placeholder="Token"/>
+                <button>Sign up</button>
             </div>
         </div>
     );
